a0e78bb7d96841c402897a5531f6ea7529059424
Ontheroad123 <31181026+Ontheroad123@users.noreply.github.com>
2018-08-25 08:58:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -23,9 +23,9 @@ def endwith(s,*endstring):
        return False
 
 #将训练集图片地址全部写入txt文件中
-def write_train_imgpath():
-    path="/home/hq/桌面/cat_dog/c-d-data/train"
-    fp = open('path_train.txt', 'w')
+def write_imgpath():
+    path="/home/hq/desktop/cat_dog/c-d-data/train"
+    fp = open('path.txt', 'w')
     for file in os.listdir(path):
         file_path = os.path.join(path, file)
         for sub_file in os.listdir(file_path):
@@ -35,7 +35,9 @@ def write_train_imgpath():
     fp.close()
 #write_train_imgpath()
 def write_test_imgpath():
-    path1='path_train.txt'
+    write_imgpath()
+    path1='path.txt'
+    fp1 = open('path_train.tst','w')
     fp2 = open('path_test.txt','w')
     count=0
     with open(path1) as f:
@@ -46,17 +48,9 @@ def write_test_imgpath():
                 fp2.write(line+"\n")
             elif count>=22501:
                 fp2.write(line+"\n")
-
-
-#这里偷懒将训练集一部分地址直接放到path_test.txt文件中了
-'''def write_test_imgpath():
-    path = "/home/hq/桌面/cat_dog/c-d-data/test"
-    fp = open('path_test.txt', 'w')
-    for file in os.listdir(path):
-        image_path = (os.path.join(path, file))
-        fp.write(image_path + "\n")
-    fp.close()
-'''
+            else:
+                fp1.write(line+"\n")
+write_test_imgpath()
 
 #传入batch_size和需要打开的文件地址
 def generator_data(batch_size,path):
@@ -81,7 +75,7 @@ def generator_data(batch_size,path):
 def process_line(line):
 
     img = cv2.imread(line)
-
+    #print(line)
     img = cv2.resize(img, (255, 255), interpolation=cv2.INTER_CUBIC)
     str=line.split('.')
     str1=str[0].split('/')
@@ -140,18 +134,64 @@ def VGG_16():
 
     return model
 
+def build_model():
+    model = Sequential()
+    model.add(
+        Conv2D(
+            filters=32,
+            kernel_size=(3, 3),
+            padding='same',
+            dim_ordering='tf',
+            input_shape=(255,255,3),  # 输入三维
+        )
+    )
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(
+        MaxPooling2D(
+            pool_size=(2, 2),
+            strides=(2, 2),
+            padding='same'
+        )
+    )
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Dropout(0.15))
+
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same'))
+    model.add(Dropout(0.15))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(128))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(2))
+    model.add(BatchNormalization())
+    model.add(Activation('softmax'))
+    model.summary()
+    return model
+
 model = VGG_16()
 sgd = SGD(lr=0.000001,decay=1e-6,momentum=0.9,nesterov=True)
 model.compile(optimizer=sgd,  loss='squared_hinge',  metrics=['accuracy'])
 #epoch=n*steps_per_epoch*batch_size,自己理解的这样，在设置batch_size和steps的时候注意
-'''for x,y in generator_data(10,"/home/hq/桌面/cat_dog/path.txt"):
-    print(y)
-    break'''
-model.fit_generator(generator_data(32, "/home/hq/桌面/cat_dog/path_train.txt"),
+model.fit_generator(generator_data(32, "/home/hq/desktop/cat_dog/path_train.txt"),
                     steps_per_epoch=20,
                     epochs=10
                     )
-loss ,accuracy= model.evaluate_generator(generator_data(32, "/home/hq/桌面/cat_dog/path_test.txt"), steps=20)
+loss ,accuracy= model.evaluate_generator(generator_data(32, "/home/hq/desktop/cat_dog/path_test.txt"), steps=20)
 print("loss is :",loss)
 print("accuracy is :",accuracy)
 
